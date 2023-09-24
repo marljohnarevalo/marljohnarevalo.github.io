f6a4d77dd283d4b2f54653e40734b43102578a26
Marl John Arevalo <marljohnarevalohehe@gmail.com>
2023-09-24 21:43:10 +0800

    created new version

--- app.js
+++ app.js
@@ -1,34 +1,17 @@
-// FOR TESTDOME CERTIFICATES
-var stylesheet = "https://www.testdome.com/content/source/stylesheets/embed.css",
-link = document.createElement("link");
-link.href = stylesheet,
-link.type = "text/css",
-link.rel = "stylesheet",
-link.media = "screen,print",
-document.getElementsByTagName("head")[0].appendChild(link);
-// END FOR TESTDOME CERTIFICATES
-
-// FOR HEADER
-let navbar = document.querySelector(".nav-items")
 let hamburger = document.querySelector(".hamburger")
-let navItems = document.querySelectorAll(".nav-items .item")
+let nav = document.querySelector("nav")
 
-hamburger.addEventListener("click", openNavbar)
-navItems.forEach(item => {
-    item.addEventListener("click", openNavbar)
-})
+function toggleNav(){
+    nav.classList.toggle("hidden")
 
-function openNavbar(){
-    if(!navbar.classList.contains("visible")){
-        navbar.classList.add("visible")
-        navbar.classList.remove("invisible")
-        hamburger.classList.add("visible")
-        hamburger.classList.remove("invisible")
+    if(!nav.classList.contains("hidden")){
+        hamburger.classList.add("effect")
     }
     else{
-        navbar.classList.remove("visible")
-        navbar.classList.add("invisible")
-        hamburger.classList.remove("visible")
-        hamburger.classList.add("invisible")
+        hamburger.classList.remove("effect")
     }
 }
+
+hamburger.addEventListener("click", toggleNav)
+
+
